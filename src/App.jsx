41d6df619867b84db880649d48f3b732e69a6650
Isaac Ho <Isaachwho@gmail.com>
2024-01-25 11:48:11 -0800

    created basic form structure with todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  // inside react you can't update state, it is immutable
+  const [newItems, setNewItem] = useState("");
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item"></input>
-      </div>
-    </form>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"></input>
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
